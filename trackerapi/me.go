2e52ead2691dd9bf019154193fd14bba3c0d5f51
Jan Alexandra Katz <beeswaxboffin@gmail.com>
2020-05-03 08:52:38 -0400

    Internal changes for new repo address

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/jkatzmewing/clirescue/cmdutil"
+	"github.com/jkatzmewing/clirescue/user"
 )
 
 var (
